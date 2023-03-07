9ba4a44a3f1480b9a95787311fd17b6f5df60b66
Maximilian Stölzle <maximilian@stoelzle.ch>
2023-03-07 16:51:32 +0100

    Add processing and batching of data to `load_dataset`

--- examples/training/load_dataset.py
+++ examples/training/load_dataset.py
@@ -1,6 +1,14 @@
 from jax import random
 
-from src.training.load_dataset import load_datasets
+from src.training.load_dataset import load_dataset
 
 if __name__ == "__main__":
-    load_datasets("mechanical_system/single_pendulum")
+    datasets = load_dataset(
+        "mechanical_system/single_pendulum",
+        batch_size=32,
+        normalize=True,
+        grayscale=True
+    )
+    print("train_ds: ", datasets["train"])
+    print("val_ds: ", datasets["val"])
+    print("test_ds: ", datasets["test"])
--- src/training/load_dataset.py
+++ src/training/load_dataset.py
@@ -6,19 +6,25 @@ import tensorflow_datasets as tfds
 from typing import Dict, Tuple
 
 
-def load_datasets(
+def load_dataset(
     name: str,
+    batch_size: int,
     val_perc: int = 20,
     test_perc: int = 20,
-) -> Tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
+    normalize: bool = True,
+    grayscale: bool = False,
+) -> Dict[str, tf.data.Dataset]:
     """
-    Loads the dataset and splits it into a training, validation and test dataset.
+    Loads the dataset and splits it into a training, validation and test set.
     Args:
         name: Name of the dataset. Can also be name/config_name:version.
             https://www.tensorflow.org/datasets/api_docs/python/tfds/load
             Example: "mechanical_system/single_pendulum"
+        batch_size: Batch size of the dataset.
         val_perc: Percentage of validation dataset with respect to the entire dataset size. Needs to be in interval [0, 100].
         test_perc: Percentage of single_pendulum dataset with respect to the entire dataset size. Needs to be in interval [0, 100].
+        normalize: Whether to normalize the rendering image to [0, 1].
+        grayscale: Whether to convert the rendering image to grayscale.
     Returns:
         train_ds: trainings set.
         val_ds: validation set.
@@ -28,7 +34,8 @@ def load_datasets(
     assert 0 <= test_perc <= 100, "Test ratio needs to be in interval [0, 100]."
     assert val_perc + test_perc <= 100, "The sum of validation and test ratio need to be equal or smaller than 100."
 
-    train_ds, val_ds, test_ds = tfds.load(
+    datasets = {}
+    datasets["train"], datasets["val"], datasets["test"] = tfds.load(
         name,
         data_dir=Path("data/tensorflow_datasets"),
         split=[
@@ -38,4 +45,22 @@ def load_datasets(
         ]
     )
 
-    return train_ds, val_ds, test_ds
+    for split_name in datasets.keys():
+        ds = datasets[split_name]
+
+        if grayscale:
+            # convert rendering image to grayscale
+            ds = ds.map(lambda sample: sample | {
+                "rendering_ts": tf.image.rgb_to_grayscale(sample["rendering_ts"]),
+            })
+
+        if normalize:
+            # normalize rendering image to [0, 1]
+            ds = ds.map(lambda sample: sample | {
+                "rendering_ts": tf.cast(sample["rendering_ts"], tf.float32) / 255.0,
+            })
+
+        # group into batches of batch_size and skip incomplete batch, prefetch the next sample to improve latency
+        datasets[split_name] = ds.batch(batch_size, drop_remainder=True).prefetch(1)
+
+    return datasets
