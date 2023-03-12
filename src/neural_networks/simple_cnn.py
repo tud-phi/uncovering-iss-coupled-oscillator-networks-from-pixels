aa53b35c35da8528084f24d36b0ef8e2945cb5cb
Maximilian Stölzle <maximilian@stoelzle.ch>
2023-03-12 23:10:57 +0100

    Mad nonlinearity of simple cnn a selectable argument

--- src/neural_networks/simple_cnn.py
+++ src/neural_networks/simple_cnn.py
@@ -1,21 +1,23 @@
 from flax import linen as nn  # Linen API
+from typing import Callable
 
 
 class Encoder(nn.Module):
     """A simple CNN encoder."""
     latent_dim: int
+    nonlinearity: Callable = nn.leaky_relu
 
     @nn.compact
     def __call__(self, x):
         x = nn.Conv(features=32, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
+        x = self.nonlinearity(x)
         x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
+        x = self.nonlinearity(x)
 
         x = x.reshape((x.shape[0], -1))  # flatten
 
         x = nn.Dense(features=256)(x)
-        x = nn.relu(x)
+        x = self.nonlinearity(x)
         x = nn.Dense(features=self.latent_dim)(x)
 
         return x
@@ -24,19 +26,20 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     """A simple CNN decoder."""
     latent_dim: int
+    nonlinearity: Callable = nn.leaky_relu
 
     @nn.compact
     def __call__(self, x):
         x = nn.Dense(features=self.latent_dim)(x)
-        x = nn.relu(x)
+        x = self.nonlinearity(x)
         x = nn.Dense(features=256)(x)
 
         x = x.reshape((x.shape[0], 16, 16, 1))  # flatten
 
         x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
+        x = self.nonlinearity(x)
         x = nn.Conv(features=32, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
+        x = self.nonlinearity(x)
 
         return x
 
@@ -44,10 +47,11 @@ class Decoder(nn.Module):
 class Autoencoder(nn.Module):
     """A simple CNN autoencoder."""
     latent_dim: int
+    nonlinearity: Callable = nn.leaky_relu
 
     def setup(self):
-        self.encoder = Encoder(latent_dim=self.latent_dim)
-        self.decoder = Decoder(latent_dim=self.latent_dim)
+        self.encoder = Encoder(latent_dim=self.latent_dim, nonlinearity=self.nonlinearity)
+        self.decoder = Decoder(latent_dim=self.latent_dim, nonlinearity=self.nonlinearity)
 
     def __call__(self, x):
         x = self.encoder(x)
