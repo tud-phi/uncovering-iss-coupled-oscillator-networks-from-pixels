477595f6a1b353bdc4b3108f0d5461f85fc3e3e0
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-05-03 11:41:00 +0200

    Split autoencoding into training and evaluation scripts

--- examples/evaluation/eval_single_pendulum_autoencoding.py
+++ examples/evaluation/eval_single_pendulum_autoencoding.py
@@ -12,6 +12,7 @@ from src.neural_networks.convnext import ConvNeXtAutoencoder
 from src.tasks import autoencoding
 from src.training.load_dataset import load_dataset
 from src.training.loops import run_training, run_eval
+from src.training.train_state_utils import restore_train_state
 
 # prevent tensorflow from loading everything onto the GPU, as we don't have enough memory for that
 tf.config.experimental.set_visible_devices([], "GPU")
@@ -20,7 +21,7 @@ tf.config.experimental.set_visible_devices([], "GPU")
 seed = 0
 rng = random.PRNGKey(seed=seed)
 
-use_wae = False
+use_wae = True
 sigma_z = 1.0
 
 num_epochs = 25
@@ -31,17 +32,14 @@ if use_wae:
     normalize_latent_space = False
     batch_size = 10
     loss_weights = dict(mse_q=0.0, mse_rec=5.0, mmd=1.0)
-    base_lr = 1e-3
 else:
     latent_dim = 2
     normalize_latent_space = True
     batch_size = 8
     loss_weights = dict(mse_q=1.0, mse_rec=5.0)
-    base_lr = 5e-3
 
-now = datetime.now()
-logdir = Path("logs") / "single_pendulum_autoencoding" / f"{now:%Y-%m-%d_%H-%M-%S}"
-logdir.mkdir(parents=True, exist_ok=True)
+sym_exp_filepath = Path("symbolic_expressions") / "single_pendulum.dill"
+ckpt_dir = Path("logs") / "single_pendulum_autoencoding" / "2023-05-03_11-11-05"
 
 if __name__ == "__main__":
     datasets, dataset_info, dataset_metadata = load_dataset(
@@ -71,19 +69,30 @@ if __name__ == "__main__":
         sigma_z=sigma_z,
     )
 
-    # run the training loop
-    print("Run training...")
-    (state, train_history,) = run_training(
-        rng=rng,
-        train_ds=train_ds,
-        val_ds=val_ds,
-        task_callables=task_callables,
-        metrics=metrics,
-        num_epochs=num_epochs,
-        nn_model=nn_model,
-        base_lr=base_lr,
-        warmup_epochs=warmup_epochs,
-        weight_decay=0.0,
-        logdir=logdir,
+    state = restore_train_state(rng, ckpt_dir, nn_model, metrics)
+
+    print("Run testing...")
+    test_history = run_eval(test_ds, state, task_callables)
+    rmse_q_stps, rmse_rec_stps = test_history.collect("rmse_q", "rmse_rec")
+    print(
+        f"Final test metrics: rmse_q={rmse_q_stps[-1]:.3f}, rmse_rec={rmse_rec_stps[-1]:.3f}"
     )
-    print("Final training metrics:\n", state.metrics.compute())
+
+    test_batch = next(test_ds.as_numpy_iterator())
+    test_preds = task_callables.forward_fn(test_batch, state.params)
+
+    import matplotlib.pyplot as plt
+
+    for i in range(test_batch["x_ts"].shape[0]):
+        print("test sample:", i)
+        img_gt = (128 * (1.0 + test_batch["rendering_ts"][i, 0])).astype(jnp.uint8)
+        img_rec = (128 * (1.0 + test_preds["rendering_ts"][i, 0])).astype(jnp.uint8)
+
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+        img_gt_plot = axes[0].imshow(img_gt, vmin=0, vmax=255)
+        plt.colorbar(img_gt_plot, ax=axes[0])
+        axes[0].set_title("Original")
+        img_rec_plot = axes[1].imshow(img_rec, vmin=0, vmax=255)
+        plt.colorbar(img_rec_plot, ax=axes[1])
+        axes[1].set_title("Reconstruction")
+        plt.show()
