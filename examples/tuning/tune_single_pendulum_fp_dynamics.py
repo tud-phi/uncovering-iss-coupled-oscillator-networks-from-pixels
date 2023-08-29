0dff83c6413341559eafff8bb107cde523ceb48a
Maximilian Stölzle <maximilian@stoelzle.ch>
2023-08-29 15:43:53 +0200

    Implement optuna script for tuning fp dynamics parameters

--- examples/tuning/tune_single_pendulum_fp_dynamics.py
+++ examples/tuning/tune_single_pendulum_fp_dynamics.py
@@ -3,17 +3,17 @@ import dill
 from jax import random
 from jax import config as jax_config
 import jax.numpy as jnp
-from jsrm.systems.pendulum import normalize_joint_angles
+from jsrm.integration import ode_factory
+from jsrm.systems.pendulum import factory, normalize_joint_angles
 import logging
 from pathlib import Path
 import optuna
 from optuna.samplers import TPESampler
 import tensorflow as tf
 
-from src.neural_networks.convnext import ConvNeXtAutoencoder
 from src.neural_networks.simple_cnn import Autoencoder
 from src.neural_networks.vae import VAE
-from src.tasks import autoencoding
+from src.tasks import fp_dynamics
 from src.training.callbacks import OptunaPruneCallback
 from src.training.load_dataset import load_dataset
 from src.training.loops import run_training, run_eval
@@ -26,22 +26,21 @@ tf.config.experimental.set_visible_devices([], "GPU")
 seed = 0
 rng = random.PRNGKey(seed=seed)
 
-multi_objective = False  # whether to use a multi-objective optuna study
-rec_loss_type = "mse"
 ae_type = "beta_vae"
 
-latent_dim = 2
 normalize_latent_space = True
 max_num_epochs = 50
 warmup_epochs = 5
 batch_size = 100
 
 now = datetime.now()
-experiment_name = "single_pendulum_autoencoding"
+experiment_name = "single_pendulum_fp_dynamics"
 datetime_str = f"{now:%Y-%m-%d_%H-%M-%S}"
 study_id = f"study-{experiment_name}-{datetime_str}"  # Unique identifier of the study.
 logdir = Path("logs") / experiment_name / datetime_str
 logdir.mkdir(parents=True, exist_ok=True)
+
+sym_exp_filepath = Path("symbolic_expressions") / "single_pendulum.dill"
 
 if __name__ == "__main__":
     datasets, dataset_info, dataset_metadata = load_dataset(
@@ -53,65 +52,77 @@ if __name__ == "__main__":
     )
     train_ds, val_ds, test_ds = datasets["train"], datasets["val"], datasets["test"]
 
+    # extract the robot parameters from the dataset
+    robot_params = dataset_metadata["system_params"]
+    print(f"Robot parameters: {robot_params}")
+
     # dimension of the latent space
     n_q = train_ds.element_spec["x_ts"].shape[-1] // 2
+    # latent space shape
+    latent_dim = 2 * n_q
     # image shape
     img_shape = train_ds.element_spec["rendering_ts"].shape[-3:]
 
+    # get the dynamics function
+    forward_kinematics_fn, dynamical_matrices_fn = factory(sym_exp_filepath)
+
     # initialize the model
-    if ae_type == "beta_vae":
-        nn_model = VAE(
-            latent_dim=latent_dim,
-            img_shape=img_shape,
-            nonlinearity=jnp.tanh,
-            clip_decoder_output=False,
-        )
-    else:
-        nn_model = Autoencoder(latent_dim=latent_dim, img_shape=img_shape)
+    nn_model = Autoencoder(latent_dim=latent_dim, img_shape=img_shape)
 
     # define the objective function for hyperparameter tuning
     def objective(trial):
         # Sample hyperparameters
-        base_lr = trial.suggest_float("base_lr", 1e-5, 1e-2, log=True)
-        beta = trial.suggest_float("beta", 1e-4, 1e1, log=True)
-        b1 = trial.suggest_float("b1", 0.9, 0.999, log=False)  # default b1 = 0.9
-        b2 = trial.suggest_float("b2", 0.999, 0.9999, log=False)  # default b2 = 0.999
-        weight_decay = trial.suggest_float("weight_decay", 1e-7, 1e-2, log=True)
-
-        if rec_loss_type == "mse":
-            train_loss_weights = dict(mse_q=0.0, mse_rec=1.0, beta=beta)
-        elif rec_loss_type == "bce":
-            train_loss_weights = dict(mse_q=0.0, bce_rec=1.0, beta=beta)
-        else:
-            raise ValueError(f"Unknown rec_loss_type: {rec_loss_type}")
+        base_lr = trial.suggest_float("base_lr", 1e-4, 1e-2, log=True)
+        # loss weights
+        mse_q = trial.suggest_float("mse_q", 1e-2, 1e2, log=True)
+        mse_rec_static_weight = 1.0
+        mse_rec_dynamic_weight = trial.suggest_float(
+            "mse_rec_dynamic_weight", 1e-1, 5e2, log=True
+        )
+        # beta = trial.suggest_float("beta", 1e-4, 1e1, log=True)
+        b1 = 0.9
+        b2 = 0.999
+        weight_decay = trial.suggest_float("weight_decay", 1e-5, 1e-3, log=True)
+        # fp dynamics settings
+        configuration_velocity_source = trial.suggest_categorical(
+            "configuration_velocity_source",
+            ["direct-finite-differences", "image-space-finite-differences"],
+        )
+        start_time_idx = trial.suggest_int("start_time_idx", 1, 7)
+        # initialize the loss weights
+        loss_weights = dict(
+            mse_q=mse_q,
+            mse_rec_static=mse_rec_static_weight,
+            mse_rec_dynamic=mse_rec_dynamic_weight,
+        )
 
         if ae_type != "beta_vae":
             raise ValueError("Only beta_vae is supported for now")
 
-        # call the factory function for the sensing task
-        train_task_callables, train_metrics = autoencoding.task_factory(
+        # call the factory function for the task
+        task_callables, metrics = fp_dynamics.task_factory(
             "pendulum",
             nn_model,
-            loss_weights=train_loss_weights,
-            normalize_latent_space=normalize_latent_space,
-            rec_loss_type=rec_loss_type,
-            # weight_on_foreground=0.15,
-            ae_type=ae_type,
-            eval=False,
+            ode_fn=ode_factory(
+                dynamical_matrices_fn, robot_params, tau=jnp.zeros((n_q,))
+            ),
+            loss_weights=loss_weights,
+            solver=dataset_metadata["solver_class"](),
+            configuration_velocity_source=configuration_velocity_source,
+            start_time_idx=start_time_idx,
         )
 
-        callbacks = []
-        if not multi_objective:
-            prune_callback = OptunaPruneCallback(trial, metric_name="rmse_rec_val")
-            callbacks = [prune_callback]
+        # add the optuna prune callback
+        prune_callback = OptunaPruneCallback(trial, metric_name="rmse_rec_val")
+        callbacks = [prune_callback]
 
         print(f"Running trial {trial.number}...")
         (state, history, elapsed) = run_training(
             rng=rng,
             train_ds=train_ds,
             val_ds=val_ds,
-            task_callables=train_task_callables,
-            metrics=train_metrics,
+            task_callables=task_callables,
+            metrics=metrics,
             num_epochs=max_num_epochs,
             nn_model=nn_model,
             base_lr=base_lr,
@@ -139,24 +150,16 @@ if __name__ == "__main__":
 
     storage_name = f"sqlite:///{logdir}/optuna_study.db"
     sampler = TPESampler(seed=seed)  # Make the sampler behave in a deterministic way.
-    if multi_objective:
-        study = optuna.create_study(
-            study_name=study_id,
-            directions=["minimize", "minimize"],
-            sampler=sampler,
-            storage=storage_name,
-        )  # Create a new study.
-    else:
-        study = optuna.create_study(
-            study_name=study_id,
-            sampler=sampler,
-            pruner=optuna.pruners.SuccessiveHalvingPruner(),
-            storage=storage_name,
-        )  # Create a new study.
+    study = optuna.create_study(
+        study_name=study_id,
+        sampler=sampler,
+        pruner=optuna.pruners.SuccessiveHalvingPruner(),
+        storage=storage_name,
+    )  # Create a new study.
 
     print(f"Run hyperparameter tuning with storage in {storage_name}...")
     study.optimize(
-        objective, n_trials=250
+        objective, n_trials=1000
     )  # Invoke optimization of the objective function.
 
     with open(logdir / "optuna_study.dill", "wb") as f:
