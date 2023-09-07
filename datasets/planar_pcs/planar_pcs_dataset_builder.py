f64c0e762f4e7e0579d3845a0d851020ca5658f6
Maximilian Stölzle <maximilian@stoelzle.ch>
2023-09-07 17:21:35 +0200

    Tune parameters for planar pcs dataset generation

--- datasets/planar_pcs/planar_pcs_dataset_builder.py
+++ datasets/planar_pcs/planar_pcs_dataset_builder.py
@@ -16,11 +16,11 @@ class PlanarPcsDatasetConfig(tfds.core.BuilderConfig):
     img_size: tuple = (128, 128)
     num_segments: int = 1
     strain_selector: Optional[Tuple] = (None,)
-    q_max: Tuple = (3 * jnp.pi, 0.02, 0.1)
-    q_d_max: Tuple = (3 * jnp.pi, 0.02, 0.1)
+    q_max: Tuple = (10 * jnp.pi, 0.05, 0.1)
+    q_d_max: Tuple = (30 * jnp.pi, 0.15, 0.3)
     num_simulations: int = 20000
     dt: float = 5e-2
-    sim_dt: float = 5e-3
+    sim_dt: float = 1e-4
     seed: int = 0
 
 
@@ -43,8 +43,8 @@ class PlanarPcs(tfds.core.GeneratorBasedBuilder):
             img_size=(128, 128),
             num_segments=1,
             strain_selector=(True, False, False),
-            q_max=(3 * jnp.pi,),
-            q_d_max=(3 * jnp.pi,),
+            q_max=(10 * jnp.pi,),
+            q_d_max=(30 * jnp.pi,),
         ),
         PlanarPcsDatasetConfig(
             name="cs_64x64px",
@@ -140,7 +140,7 @@ class PlanarPcs(tfds.core.GeneratorBasedBuilder):
             (self.builder_config.num_segments,)
         )  # Volumetric density of Dragon Skin 20 [kg/m^3]
         # damping matrix
-        D = 1e-5 * jnp.diag(
+        D = 2e-5 * jnp.diag(
             jnp.repeat(
                 jnp.array([1e0, 1e3, 1e3]), self.builder_config.num_segments, axis=0
             ),
@@ -151,10 +151,10 @@ class PlanarPcs(tfds.core.GeneratorBasedBuilder):
             "r": 2e-2 * jnp.ones((self.builder_config.num_segments,)),
             "rho": rho,
             "g": jnp.array([0.0, -9.81]),
-            "E": 1e4
-            * jnp.ones((self.builder_config.num_segments,)),  # Elastic modulus [Pa]
-            "G": 1e3
-            * jnp.ones((self.builder_config.num_segments,)),  # Shear modulus [Pa]
+            # Elastic modulus [Pa]
+            "E": 1e4 * jnp.ones((self.builder_config.num_segments,)),
+            # Shear modulus [Pa]
+            "G": 1e3 * jnp.ones((self.builder_config.num_segments,)),
             "D": D,
         }
 
