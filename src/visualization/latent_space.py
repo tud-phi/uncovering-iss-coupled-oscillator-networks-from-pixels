ccde1460515665677ca5890a76acd1dca940185d
Maximilian Stölzle <maximilian@stoelzle.ch>
2023-09-12 20:02:29 +0200

    Enable analysis of latent space for multiple configuration variables

--- src/visualization/latent_space.py
+++ src/visualization/latent_space.py
@@ -49,37 +49,36 @@ def visualize_mapping_from_configuration_to_latent_space(
         q_ss = q_ss.at[batch_idx].set(q_bt)
         z_pred_ss = z_pred_ss.at[batch_idx].set(z_pred_bt)
 
-    if q_ss.shape[-1] > 1:
-        warnings.warn(
-            "Configuration-space has dimension > 1. Plotting only the first dimension."
-        )
-
     q_ss = q_ss.reshape((-1, q_bt.shape[-1]))
     z_pred_ss = z_pred_ss.reshape((-1, z_pred_bt.shape[-1]))
 
-    sort_indices = jnp.argsort(q_ss[:, 0])
-    q_ss_sorted = q_ss[sort_indices]
-    q_pred_ss_sorted = z_pred_ss[sort_indices]
+    # number of configuration variables
+    n_q = q_ss.shape[-1]
+    n_z = z_pred_ss.shape[-1]
 
     plt.figure(num="Histogram of configuration space")
-    for q_idx in range(q_ss_sorted.shape[-1]):
-        counts, bins = jnp.histogram(q_ss_sorted[:, q_idx], bins=50)
-        plt.stairs(counts, bins, label=f"q_{q_idx}")
-    plt.xlabel("q")
+    for q_idx in range(q_ss.shape[-1]):
+        counts, bins = jnp.histogram(q_ss[:, q_idx], bins=50)
+        plt.stairs(counts, bins, label=fr"$q_{q_idx}$")
+    plt.xlabel("$q$")
     plt.ylabel("count")
     plt.legend()
     plt.tight_layout()
     plt.show()
 
-    plt.figure(num="Mapping from configuration to latent space")
-    for latent_idx in range(q_pred_ss_sorted.shape[-1]):
-        plt.plot(
-            q_ss_sorted[:, 0],
-            q_pred_ss_sorted[:, latent_idx],
-            linestyle="None",
-            marker=".",
-        )
-    plt.xlabel("q")
-    plt.ylabel("z")
+    fig, axes = plt.subplots(nrows=n_q, ncols=1, num="Mapping from configuration to latent space")
+    for q_idx in range(n_q):
+        ax = axes[q_idx]
+        for z_idx in range(n_z):
+            ax.plot(
+                q_ss[:, q_idx],
+                z_pred_ss[:, z_idx],
+                linestyle="None",
+                marker=".",
+                label=fr"$z_{z_idx}$",
+            )
+        ax.set_xlabel(f"$q_{q_idx}$")
+        ax.set_ylabel("$z$")
+        ax.legend()
     plt.tight_layout()
     plt.show()
