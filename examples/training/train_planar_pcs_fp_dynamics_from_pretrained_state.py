587726686eebd9aaee8d684e215a90670b147bb5
Maximilian Stölzle <maximilian@stoelzle.ch>
2023-09-24 22:56:32 +0200

    Get fp dynamics training based on pretraining working

--- examples/training/train_planar_pcs_fp_dynamics_from_pretrained_state.py
+++ examples/training/train_planar_pcs_fp_dynamics_from_pretrained_state.py
@@ -28,6 +28,9 @@ from src.visualization.dataset_distribution import (
     plot_acting_forces_distribution,
     plot_basic_distribution,
 )
+from src.visualization.latent_space import (
+    visualize_mapping_from_configuration_to_latent_space,
+)
 
 # prevent tensorflow from loading everything onto the GPU, as we don't have enough memory for that
 tf.config.experimental.set_visible_devices([], "GPU")
@@ -38,38 +41,18 @@ rng = random.PRNGKey(seed=seed)
 tf.random.set_seed(seed=seed)
 
 system_type = "cc"
-ae_type = "None"  # "None", "beta_vae", "wae"
-ckpt_dir = Path("logs") / f"{system_type}_autoencoding" / f"2023-09-16_11-54-15"
+pretrained_ae_type = "beta_vae"  # "None", "beta_vae", "wae"
+ckpt_dir = Path("logs") / f"{system_type}_autoencoding" / f"2023-09-24_14-41-33"
 
-num_epochs = 25
+num_epochs = 50
 warmup_epochs = 5
 batch_size = 100
-start_time_idx = 1
-configuration_velocity_source = "direct-finite-differences"
+start_time_idx = 0
+configuration_velocity_source = "ground-truth"
 
-if ae_type == "wae":
-    base_lr = 0.005
-    loss_weights = dict(
-        mse_q=0.25,
-        mse_rec_static=1.0,
-        mse_rec_dynamic=20.0,
-        mmd=0.40,
-    )
-    weight_decay = 0.0001660747175371815
-elif ae_type == "beta_vae":
-    base_lr = 0.0034025235838459704
-    loss_weights = dict(
-        mse_q=0.2425386811791378,
-        mse_rec_static=1.0,
-        mse_rec_dynamic=37.740461886268136,
-        beta=0.025130527547712868,
-    )
-    weight_decay = 0.00010327082269198063
-else:
-    # ae_type == "None"
-    base_lr = 1e-2
-    loss_weights = dict(mse_q=0.70, mse_rec_static=1.0, mse_rec_dynamic=77.0)
-    weight_decay = 1.7e-05
+base_lr = 5e-1
+loss_weights = dict(mse_q=1e-2, mse_rec_static=0.0, mse_rec_dynamic=100)
+weight_decay = 0.0
 
 now = datetime.now()
 logdir = Path("logs") / f"{system_type}_fp_dynamics" / f"{now:%Y-%m-%d_%H-%M-%S}"
@@ -112,7 +95,7 @@ if __name__ == "__main__":
     # plot_acting_forces_distribution(train_ds, system_type, robot_params, dynamical_matrices_fn)
 
     # initialize the model
-    if ae_type == "beta_vae":
+    if pretrained_ae_type == "beta_vae":
         backbone = VAE(
             latent_dim=latent_dim, img_shape=img_shape, norm_layer=nn.LayerNorm
         )
@@ -130,7 +113,7 @@ if __name__ == "__main__":
         ts=dataset_metadata["ts"],
         sim_dt=dataset_metadata["sim_dt"],
         loss_weights=loss_weights,
-        ae_type=ae_type,
+        ae_type="None",
         solver=dataset_metadata["solver_class"](),
         start_time_idx=start_time_idx,
         configuration_velocity_source=configuration_velocity_source,
@@ -199,3 +182,7 @@ if __name__ == "__main__":
     )
     print("Final training metrics:\n", state.metrics.compute())
     print("Final params of head:\n", state.params["head"])
+
+    visualize_mapping_from_configuration_to_latent_space(
+        test_ds, state, task_callables, rng=rng
+    )
