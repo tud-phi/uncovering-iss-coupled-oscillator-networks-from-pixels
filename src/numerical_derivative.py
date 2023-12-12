f222b84df48c47f0f482942478b8a8f7408b3597
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-12-12 17:57:37 +0100

    Remove debugging prints

--- src/numerical_derivative.py
+++ src/numerical_derivative.py
@@ -17,8 +17,6 @@ def savotzky_golay_scipy(dt: float, img_ts: Array, window_length: int = 3, polyo
         img_dd_ts: numerical 2nd derivative of image time series
     """
     img_d_ts = savgol_filter(img_ts, window_length=window_length, polyorder=polyorder, deriv=1, delta=dt, axis=0)
-    print("1st derivative", "min", img_d_ts[0].min(), "max", img_d_ts[0].max())
     img_dd_ts = savgol_filter(img_ts, window_length=window_length, polyorder=polyorder, deriv=2, delta=dt, axis=0)
-    print("2nd derivative", "min", img_dd_ts[0].min(), "max", img_dd_ts[0].max())
 
     return img_d_ts, img_dd_ts
