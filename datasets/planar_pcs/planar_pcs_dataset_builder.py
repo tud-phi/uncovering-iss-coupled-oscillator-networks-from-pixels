adab699604afcdfb4f464e8f2155fc9d13fbaed3
Maximilian Stölzle <maximilian@stoelzle.ch>
2024-02-05 18:03:55 +0100

    Increase `dt` for planar pcs simulation

--- datasets/planar_pcs/planar_pcs_dataset_builder.py
+++ datasets/planar_pcs/planar_pcs_dataset_builder.py
@@ -19,7 +19,7 @@ class PlanarPcsDatasetConfig(tfds.core.BuilderConfig):
     q_max: Tuple = (10 * jnp.pi, 0.05, 0.1)
     q_d_max: Tuple = (10 * jnp.pi, 0.05, 0.1)
     num_simulations: int = 20000
-    dt: float = 5e-3
+    dt: float = 2e-2
     sim_dt: float = 1e-3
     seed: int = 0
 
@@ -271,5 +271,5 @@ class PlanarPcs(tfds.core.GeneratorBasedBuilder):
             metadata=metadata,
             x0_sampling_dist="uniform",
             tau_max=tau_max,
-            save_raw_data=True
+            save_raw_data=False
         )
