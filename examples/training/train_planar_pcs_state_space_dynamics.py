bbd23db673de8ec131c423af37cf6e8da8fbe847
Maximilian Stölzle <maximilian@stoelzle.ch>
2024-02-06 17:12:03 +0100

    Add script for training state space dynamics on planar pcs system

--- examples/training/train_planar_pcs_state_space_dynamics.py
+++ examples/training/train_planar_pcs_state_space_dynamics.py
@@ -6,7 +6,7 @@ jax_config.update("jax_enable_x64", True)
 from jax import random
 import jax.numpy as jnp
 import jsrm
-from jsrm.systems import pendulum
+from jsrm.systems import planar_pcs
 from pathlib import Path
 import tensorflow as tf
 
@@ -26,6 +26,7 @@ seed = 0
 rng = random.PRNGKey(seed=seed)
 tf.random.set_seed(seed=seed)
 
+system_type = "pcc_ns-2"
 # dynamics_model_name in ["node-general-mlp", "node-mechanical-mlp", "node-cornn", "node-con", "node-lnn", "node-hippo-lss", "discrete-mlp"]
 dynamics_model_name = "node-mechanical-mlp"
 
@@ -48,6 +49,15 @@ weight_decay = 0.0
 num_mlp_layers = 4
 mlp_hidden_dim = 40
 
+# identify the number of segments
+if system_type == "cc":
+    num_segments = 1
+elif system_type.split("_")[0] == "pcc":
+    num_segments = int(system_type.split("-")[-1])
+else:
+    raise ValueError(f"Unknown system_type: {system_type}")
+print(f"Number of segments: {num_segments}")
+
 # identify the dynamics_type
 dynamics_type = dynamics_model_name.split("-")[0]
 assert dynamics_type in ["node", "discrete"], f"Unknown dynamics_type: {dynamics_type}"
@@ -55,18 +65,18 @@ assert dynamics_type in ["node", "discrete"], f"Unknown dynamics_type: {dynamics
 now = datetime.now()
 logdir = (
     Path("logs").resolve()
-    / "single_pendulum_state_space_dynamics"
+    / f"{system_type}_state_space_dynamics"
     / f"{now:%Y-%m-%d_%H-%M-%S}"
 )
 logdir.mkdir(parents=True, exist_ok=True)
 
 sym_exp_filepath = (
-    Path(jsrm.__file__).parent / "symbolic_expressions" / f"pendulum_nl-1.dill"
+    Path(jsrm.__file__).parent / "symbolic_expressions" / f"planar_pcs_ns-{num_segments}.dill"
 )
 
 if __name__ == "__main__":
     datasets, dataset_info, dataset_metadata = load_dataset(
-        "pendulum/single_pendulum_32x32px",
+        f"planar_pcs/{system_type}_32x32px",
         seed=seed,
         batch_size=batch_size,
         normalize=True,
@@ -77,13 +87,16 @@ if __name__ == "__main__":
     # extract the robot parameters from the dataset
     robot_params = dataset_metadata["system_params"]
     print(f"Robot parameters: {robot_params}")
+    print("Strain selector:", dataset_metadata["strain_selector"])
     n_tau = train_ds.element_spec["tau"].shape[-1]  # dimension of the control input
     n_q = (
         train_ds.element_spec["x_ts"].shape[-1] // 2
     )  # dimension of the configuration space
 
     # get the dynamics function
-    forward_kinematics_fn, dynamical_matrices_fn = pendulum.factory(sym_exp_filepath)
+    strain_basis, forward_kinematics_fn, dynamical_matrices_fn = planar_pcs.factory(
+        sym_exp_filepath, strain_selector=dataset_metadata["strain_selector"]
+    )
 
     if dynamics_model_name in ["node-general-mlp", "node-mechanical-mlp"]:
         nn_model = MlpOde(
@@ -142,10 +155,10 @@ if __name__ == "__main__":
 
     # call the factory function for the state space dynamics task
     task_callables, metrics_collection_cls = state_space_dynamics.task_factory(
-        "pendulum",
+        system_type,
         nn_model,
         ts=dataset_metadata["ts"],
-        sim_dt=dataset_metadata["sim_dt"],
+        sim_dt=jnp.min(jnp.diff(dataset_metadata["ts"])).item() / 4,
         loss_weights=loss_weights,
         dynamics_type=dynamics_type,
         start_time_idx=start_time_idx,
