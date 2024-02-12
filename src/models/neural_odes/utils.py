9ef9a07c4d1424ef4125bb1b0974d87220280fe9
Maximilian Stölzle <maximilian@stoelzle.ch>
2024-02-12 15:58:23 +0100

    Massively speed-up `generate_positive_definite_matrix_from_params`

--- src/models/neural_odes/utils.py
+++ src/models/neural_odes/utils.py
@@ -10,12 +10,14 @@ def generate_positive_definite_matrix_from_params(
     """
     Generate a positive definite matrix of shape (n, n) from a vector of parameters.
     Args:
-        params: A vector of parameters of shape ((n^2 + n) / 2, ).
+        n: Number of rows and columns of the matrix.
+        a: A vector of parameters of shape ((n^2 + n) / 2, ).
         diag_shift: A small value that is added to the diagonal entries of the matrix before the softplus.
         diag_eps: A small value that is added to the diagonal entries of the matrix after the softplus.
     Returns:
         A: A positive definite matrix of shape (n, n).
     """
+    """ Naive (i.e., slow) implementation
     # construct empty triangular matrix
     tril_mat = jnp.zeros((n, n))
     # (i, j) indices of lower triangular matrix
@@ -28,8 +30,26 @@ def generate_positive_definite_matrix_from_params(
     tril_mat = tril_mat.at[diag_indices].set(
         nn.softplus(tril_mat[diag_indices] + diag_shift) + diag_eps
     )
-
     # construct mass matrix from triangular matrix
     A = tril_mat @ tril_mat.transpose()
+    """
+
+    # construct upper triangular matrix
+    # https://github.com/google/jax/discussions/10146
+    u = jnp.concatenate([a, a[n:][::-1]])
+    U = u.reshape((n, n))
+
+    # Set the elements below the diagonal to zero
+    U = jnp.triu(U, k=0)
+
+    # make sure that the diagonal entries are positive
+    u_diag = jnp.diag(U)
+    # apply shift, softplus, and epsilon
+    new_u_diag = nn.softplus(u_diag + diag_shift) + diag_eps
+    # update diagonal
+    U = U - jnp.diag(u_diag) + jnp.diag(new_u_diag)
+
+    # reverse Cholesky decomposition
+    A = U.transpose() @ U
 
     return A
