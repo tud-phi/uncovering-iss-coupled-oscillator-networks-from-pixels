2413f4240ba13927cd923680f215457be9e245cc
Maximilian Stolzle <maximilian@stoelzle.ch>
2024-05-06 10:04:21 +0200

    Start working on implementing and approximate closed form solution for con

--- examples/closed_form_solution/test_con_closed_form.py
+++ examples/closed_form_solution/test_con_closed_form.py
@@ -4,11 +4,13 @@ jax.config.update("jax_enable_x64", True)  # double precision
 jax.config.update("jax_platform_name", "cpu")  # use CPU
 from diffrax import diffeqsolve, ODETerm, SaveAt, Tsit5
 from functools import partial
+from jax import lax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+from typing import Dict, Tuple
 
 # time steps
-dt = 1e-4
+dt = jnp.array(1e-4)
 ts = jnp.arange(0.0, 60.0, dt)
 
 # parameters
@@ -18,18 +20,15 @@ gamma = 0.1 * jnp.ones((num_units, ))  # stiffness
 epsilon = 0.05 * jnp.ones((num_units, ))  # damping coefficient
 match num_units:
     case 1:
-        W = jnp.array([[1.0]])  # coupling matrix
-        b = jnp.array([-0.5])  # bias
+        W = 1e-1 * jnp.array([[1.0]])  # coupling matrix
+        b = 1e-1 * jnp.array([-0.5])  # bias
+        # W = jnp.array([[0.0]])
+        # b = jnp.array([0.0])
     case 2:
         W = jnp.array([[1.0, 0.0], [0.0, 1.0]])
         b = jnp.array([-0.5, 0.5])
     case _:
         raise NotImplementedError
-
-# natural frequency
-omega_n = jnp.sqrt(gamma / m)
-# damping ratio
-zeta = epsilon / (2 * jnp.sqrt(m * gamma))
 
 
 def lecun_tanh(x: jax.Array) -> jax.Array:
@@ -65,55 +64,132 @@ def ode_fn(
     return y_d
 
 
-def closed_form(
+def closed_form_approximation_step_no_damping(
         t: jax.Array,
+        t0: jax.Array,
         y0: jax.Array,
         m: jax.Array,
         gamma: jax.Array,
-        epsilon: jax.Array,
-        W: jax.Array,
-        b: jax.Array
+        f_ext: jax.Array,
 ) -> jax.Array:
     """
     Closed-form solution of the harmonic oscillator with underdamping.
     https://scholar.harvard.edu/files/schwartz/files/lecture1-oscillators-and-linearity.pdf
     Args:
         t: time
+        t0: initial time
         y0: initial state
         m: mass
         gamma: stiffness
         epsilon: damping
-        W: coupling matrix
-        b: bias
+        f_ext: external force
     Returns:
         y: oscillator state at time t
     """
     x0, v0 = jnp.split(y0, 2)
 
-    if jnp.all(epsilon == 0.0):
-        x = x0*jnp.cos(jnp.sqrt(gamma)*t/jnp.sqrt(m)) + jnp.sqrt(m)*v0*jnp.sin(jnp.sqrt(gamma)*t/jnp.sqrt(m))/jnp.sqrt(gamma)
-        x_d = -jnp.sqrt(gamma)*x0*jnp.sin(jnp.sqrt(gamma)*t/jnp.sqrt(m))/jnp.sqrt(m) + v0*jnp.cos(jnp.sqrt(gamma)*t/jnp.sqrt(m))
-    elif jnp.all(epsilon < 2*jnp.sqrt(m*gamma)):
-        print("Underdamped oscillators")
-        # https://tttapa.github.io/Pages/Arduino/Audio-and-Signal-Processing/VU-Meters/Damped-Harmonic-Oscillator.html
-        alpha = zeta * omega_n
-        beta = omega_n * jnp.sqrt(1 - zeta**2)
-        lambda1, lambda2 = -alpha + beta * 1j, -alpha - beta * 1j
-        # constants for the closed-form solution
-        c1 = (lambda2 * x0 - v0) / (lambda2 - lambda1)
-        c2 = (v0 - lambda1 * x0) / (lambda2 - lambda1)
-        ctilde1 = c1 + c2
-        ctilde2 = (c1-c2) * 1j
-
-        ff1 = lecun_tanh(W @ x0 + b).squeeze(-1)
-
-        x = (ctilde1*jnp.cos(beta*t) + ctilde2*jnp.sin(beta*t)) * jnp.exp(-(alpha * t + jnp.abs(ff1))) * ff1
-        x_d = -((ctilde1*alpha - ctilde2*beta)*jnp.cos(beta*t) + (ctilde1*beta + ctilde2*alpha)*jnp.sin(beta*t)) * jnp.exp(-alpha * t + jnp.abs(ff1))
-    else:
-        raise NotImplementedError
+    x = x0*jnp.cos(jnp.sqrt(gamma)*(t-t0)/jnp.sqrt(m)) + jnp.sqrt(m)*v0*jnp.sin(jnp.sqrt(gamma)*(t-t0)/jnp.sqrt(m))/jnp.sqrt(gamma)
+    x_d = -jnp.sqrt(gamma)*x0*jnp.sin(jnp.sqrt(gamma)*(t-t0)/jnp.sqrt(m))/jnp.sqrt(m) + v0*jnp.cos(jnp.sqrt(gamma)*(t-t0)/jnp.sqrt(m))
 
     y = jnp.concatenate([x, x_d])
     return y
+
+
+def closed_form_approximation_step_underdamping(
+        t: jax.Array,
+        t0: jax.Array,
+        y0: jax.Array,
+        m: jax.Array,
+        gamma: jax.Array,
+        epsilon: jax.Array,
+        f_ext: jax.Array,
+) -> jax.Array:
+    """
+    Closed-form solution of the harmonic oscillator with underdamping.
+    https://scholar.harvard.edu/files/schwartz/files/lecture1-oscillators-and-linearity.pdf
+    Args:
+        t: time
+        t0: initial time
+        y0: initial state
+        m: mass
+        gamma: stiffness
+        epsilon: damping
+        f_ext: external force
+    Returns:
+        y: oscillator state at time t
+    """
+    x0, v0 = jnp.split(y0, 2)
+
+    # natural frequency
+    omega_n = jnp.sqrt(gamma / m)
+    # damping ratio
+    zeta = epsilon / (2 * jnp.sqrt(m * gamma))
+
+    # https://tttapa.github.io/Pages/Arduino/Audio-and-Signal-Processing/VU-Meters/Damped-Harmonic-Oscillator.html
+    alpha = zeta * omega_n
+    beta = omega_n * jnp.sqrt(1 - zeta**2)
+    lambda1, lambda2 = -alpha + beta * 1j, -alpha - beta * 1j
+    # constants for the closed-form solution
+    c1 = (lambda2 * x0 - v0) / (lambda2 - lambda1)
+    c2 = (v0 - lambda1 * x0) / (lambda2 - lambda1)
+    ctilde1 = c1 + c2
+    ctilde2 = (c1-c2) * 1j
+
+    x = (ctilde1*jnp.cos(beta*(t-t0)) + ctilde2*jnp.sin(beta*(t-t0))) * jnp.exp(-(alpha * (t-t0)))
+    x_d = -((ctilde1*alpha - ctilde2*beta)*jnp.cos(beta*(t-t0)) + (ctilde1*beta + ctilde2*alpha)*jnp.sin(beta*(t-t0))) * jnp.exp(-alpha * (t-t0))
+
+    y = jnp.concatenate([x, x_d]).astype(jnp.float64)
+    return y
+
+
+def simulate_closed_form_approximation(
+    ts: jax.Array,
+    y0: jax.Array,
+    readout_dt: jax.Array,
+    m: jax.Array,
+    gamma: jax.Array,
+    epsilon: jax.Array,
+    W: jax.Array,
+    b: jax.Array
+):
+    # assume constant time step
+    sim_dt = ts[1] - ts[0]
+    ts_dt_template = jnp.arange(0.0, sim_dt+readout_dt, readout_dt)
+
+    if jnp.all(epsilon == 0.0):
+        closed_form_approximation_step_fn = partial(closed_form_approximation_step_no_damping, m=m, gamma=gamma)
+    elif jnp.all(epsilon < 2 * jnp.sqrt(m * gamma)):
+        closed_form_approximation_step_fn = partial(closed_form_approximation_step_underdamping, m=m, gamma=gamma, epsilon=epsilon)
+    else:
+        raise NotImplementedError
+
+    def approx_step_fn(carry: Dict[str, jax.Array], input: Dict[str, jax.Array]) -> Tuple[Dict[str, jax.Array], Dict[str, jax.Array]]:
+        y = carry["y"]
+        x, x_d = jnp.split(y, 2)
+
+        f_ext = -jnp.tanh(W @ x + b)
+
+        ts_dt = ts_dt_template + carry["t"]
+        f_ext_ts = jnp.repeat(f_ext[None, :], ts_dt.shape[0], axis=0)
+        y_ts_dt = jax.vmap(partial(closed_form_approximation_step_fn, t0=ts_dt[0], y0=y, f_ext=f_ext))(ts_dt)
+
+        carry = dict(t=ts_dt[-1], y=y_ts_dt[-1])
+        step_data = dict(ts=ts_dt[:-1], y_ts=y_ts_dt[:-1], f_ext_ts=f_ext_ts[:-1])
+        return carry, step_data
+
+    input_ts = dict(
+        ts=ts,
+    )
+
+    carry = dict(
+        t=ts[0],
+        y=y0,
+    )
+
+    carry, sim_ts = lax.scan(approx_step_fn, carry, input_ts)
+
+    return sim_ts
+
 
 # Define the harmonic oscillator ODE term
 ode_term = ODETerm(
@@ -136,11 +212,18 @@ sol = diffeqsolve(
 y_ts_numerical = sol.ys
 
 # evaluate the closed-form solution
-y_ts_closed_form = jax.vmap(partial(closed_form, y0=y0, m=m, gamma=gamma, epsilon=epsilon, W=W, b=b))(ts)
+ts_sim_closed_form = jnp.arange(ts[0], ts[-1], 1*dt)
+closed_form_sim_ts = simulate_closed_form_approximation(ts_sim_closed_form, y0, readout_dt=dt, m=m, gamma=gamma, epsilon=epsilon, W=W, b=b)
+for key in closed_form_sim_ts.keys():
+    closed_form_sim_ts[key] = closed_form_sim_ts[key].reshape((-1, ) + closed_form_sim_ts[key].shape[2:])
+ts_closed_form = closed_form_sim_ts["ts"]
+y_ts_closed_form = closed_form_sim_ts["y_ts"]
+print(y_ts_closed_form)
+
 
 # plot the position
 plt.plot(ts, y_ts_numerical[:, :num_units], label="Numerical solution")
-plt.plot(ts, y_ts_closed_form[:, :num_units:], label="Closed-form solution")
+plt.plot(ts_closed_form, y_ts_closed_form[:, :num_units:], label="Closed-form solution")
 plt.xlabel("Time")
 plt.ylabel("Position")
 plt.legend()
@@ -151,7 +234,7 @@ plt.show()
 
 # plot the velocity
 plt.plot(ts, y_ts_numerical[:, num_units:], label="Numerical solution")
-plt.plot(ts, y_ts_closed_form[:, num_units:], label="Closed-form solution")
+plt.plot(ts_closed_form, y_ts_closed_form[:, num_units:], label="Closed-form solution")
 plt.xlabel("Time")
 plt.ylabel("Velocity")
 plt.legend()
