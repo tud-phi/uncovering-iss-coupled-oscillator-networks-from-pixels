a3da6394ba13455a858fbd40048611fb2324e089
Maximilian Stolzle <maximilian@stoelzle.ch>
2024-09-25 15:48:38 -0400

    Add support for `img_min_val` and `img_max_val` to `preprocess_rendering`

--- src/rendering/normalization.py
+++ src/rendering/normalization.py
@@ -6,7 +6,11 @@ from typing import Union
 
 
 def preprocess_rendering(
-    rendering: Union[onp.ndarray, Array], grayscale: bool = False, normalize: bool = True
+    rendering: Union[onp.ndarray, Array],
+    grayscale: bool = False,
+    normalize: bool = True,
+    img_min_val: float = 0.0,
+    img_max_val: float = 255.0,
 ) -> Union[onp.ndarray, Array]:
     """
     Preprocesses the rendering image.
@@ -14,6 +18,8 @@ def preprocess_rendering(
         rendering: A numpy array of shape (width, height, 3) containing the rendered image.
         grayscale: A boolean flag indicating whether the rendering image is in grayscale.
         normalize: A boolean flag indicating whether to normalize the rendering image to [-1, 1].
+        img_min_val: The minimum pixel value of the rendering image.
+        img_max_val: The maximum pixel value of the rendering image.
     Returns:
         normalized_rendering: A numpy array containing the preprocessed rendered image.
     """
@@ -25,7 +31,7 @@ def preprocess_rendering(
 
     if normalize:
         # normalize rendering image to [-1, 1]
-        rendering = tf.cast(rendering, tf.float32) / 128.0 - 1.0
+        rendering = (tf.cast(rendering, tf.float32) - img_min_val) / (img_max_val - img_min_val) * 2.0 - 1.0
 
     if isinstance(input, jnp.ndarray):
         # cast image to jax array
