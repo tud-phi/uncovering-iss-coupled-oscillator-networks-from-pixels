6a9117e3fb04b1c4cdd849f98c1eee06afdd3d66
Maximilian Stolzle <maximilian@stoelzle.ch>
2024-10-09 15:13:07 -0400

    Set batch size to 30 for reaction diffusion dataset

--- examples/tuning/tune_generic_dynamics_autoencoder.py
+++ examples/tuning/tune_generic_dynamics_autoencoder.py
@@ -152,6 +152,7 @@ if __name__ == "__main__":
             dataset_type = "reaction_diffusion"
             grayscale = False
             dynamics_order = 1
+            trial_batch_size = 30
         elif system_type in [
             "mass_spring_friction",
             "mass_spring_friction_actuation",
