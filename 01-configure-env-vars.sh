1796db97a870ceed230ee390b72fc06ac6a7b397
Iván López Broceño <ivanlpzbrc01@gmail.com>
2025-11-12 15:07:48 +0100

    Adding `uv` as option to manage virtual environment (#4)
    * added uv venv instructions
    
    Signed-off-by: Iván López Broceño <ivanlpzbrc01@gmail.com>
    
    * venv lib path added to LD_LIBRARY_PATH
    
    Signed-off-by: Iván López Broceño <ivanlpzbrc01@gmail.com>
    
    ---------
    
    Signed-off-by: Iván López Broceño <ivanlpzbrc01@gmail.com>

--- 01-configure-env-vars.sh
+++ 01-configure-env-vars.sh
@@ -13,8 +13,9 @@ export PYTHONPATH="${PYTHONPATH}://src"
 # set the LD_LIBRARY_PATH to include the CUDA libraries
 # https://www.tensorflow.org/install/pip#linux
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/venv/lib/ # if using a virtual environment
 
-# mape sure that JAX runs deterministically
+# make sure that JAX runs deterministically
 # https://github.com/google/jax/issues/13672
 export XLA_FLAGS='--xla_gpu_deterministic_ops=true'
 export TF_DETERMINISTIC_OPS=1
